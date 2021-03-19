db074587f66e83dd4ff909534755f05d15d2e763
Athena <athena.partch@gmail.com>
2021-03-18 18:05:12 -0700

    add APIs

--- server/server.js
+++ server/server.js
@@ -3,13 +3,27 @@ const express = require("express");
 
 const app = express();
 
-app.get("/getRestaurants", (requ, res) => {
-    res.json({
+// Get all restaurants
+app.get("/api/v1/restaurants", (req, res) => {
+    res.status(200).json({
         status: "success",
-        restaurant: "shake shack",
+        data: {
+            restaurant: ["Shake Shack", "P.F. Changs", "Mario's Pizza"] 
+        },
     })
 });
-//http://localhost:4000/getRestaurants
+//http://localhost:4000/api/v1/restaurants
+
+
+// Get single restaurant
+app.get("/api/v1/restaurants/:id", (req, res) => {
+    console.log(req.params);
+});
+
+// Create a restaurant
+app.post("/api/v1/restaurants", (req, res) => {
+    console.log(req);
+});
 
 
 const port = process.env.PORT || 3001;
