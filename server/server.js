85bbf85cde2a62749fb9b0329cae1839ea948982
Athena <athena.partch@gmail.com>
2021-03-18 18:33:40 -0700

    set up all routes

--- server/server.js
+++ server/server.js
@@ -23,13 +23,45 @@ app.get("/api/v1/restaurants", (req, res) => {
 // Get single restaurant
 app.get("/api/v1/restaurants/:id", (req, res) => {
     console.log(req.params);
+
+    res.status(200).json({
+        status: "success",
+        data: {
+            restaurant: "mcdonalds"
+        }
+    })
 });
 
 // Create a restaurant
 app.post("/api/v1/restaurants", (req, res) => {
     console.log(req.body);
+    res.status(201).json({
+        status: "success",
+        data: {
+            restaurant: "mcdonalds"
+        }
+    })
 });
 
+//Update restaurants
+app.put("/api/v1/restaurants/:id", (req, res) => {
+    console.log(req.params.id);
+    console(req.body);
+    res.status(200).json({
+        status: "success",
+        data: {
+            restaurant: "mcdonalds"
+        }
+    });
+});
+
+
+//Delete Restaurant
+app.delete("/api/v1/restaurants/:id", (req, res) => {
+    res.status(204).json({
+        status: "success"
+    })
+});
 
 const port = process.env.PORT || 3001;
 app.listen(port, () => {
