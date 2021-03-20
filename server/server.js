3bef1310e8f4a1dc8755c8adfa4ee52cd50c01a3
Athena <athena.partch@gmail.com>
2021-03-20 08:39:31 -0700

    connect express and postgrs

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
-const morgan = require('morgan')
+const db = require('./db');
+const morgan = require('morgan');
 const app = express();
 
 
@@ -8,8 +9,10 @@ app.use(express.json());
 
 
 // Get all restaurants
-app.get("/api/v1/restaurants", (req, res) => {
-    console.log("route handler ran")
+app.get("/api/v1/restaurants", async (req, res) => {
+
+    const results = await db.query('select * from restaurants')
+    console.log(results)
     res.status(200).json({
         status: "success",
         data: {
