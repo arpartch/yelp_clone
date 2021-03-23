c29f732981efa854dda908c828e07f759f684ada
Athena <athena.partch@gmail.com>
2021-03-23 16:44:34 -0700

    add react app and react routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,19 @@
 import React from 'react';
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom"
+import home from './routes/home';
+import restaurantDetailPage from './routes/restaurantDetailPage';
+import updatePage from './routes/updatePage';
 
 const App = () => {
-    return <div>App</div>
+    return <div>
+        <Router>
+            <Switch>
+            <Route exact path="/" component={home}/>
+            <Route exact path="/restaurants/:id/update" component={updatePage}/>
+            <Route exact path="/restaurants/:id" component={restaurantDetailPage}/>
+            </Switch>
+        </Router>
+    </div>
 };
 
 export default App;
---
+++ client/src/routes/restaurantDetailPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const restaurantDetailPage = () => {
+    return (
+        <div>
+            Detail Page
+        </div>
+    )
+}
+
+export default restaurantDetailPage
