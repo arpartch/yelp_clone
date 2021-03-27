aec65da501aae611d9a8a3756536e4ff25102867
Athena <athena.partch@gmail.com>
2021-03-27 10:10:36 -0700

    add review express route

--- client/src/routes/restaurantDetailPage.jsx
+++ client/src/routes/restaurantDetailPage.jsx
@@ -1,6 +1,7 @@
 import React, { useContext, useEffect } from 'react';
 import { useParams } from 'react-router';
 import RestaurantFinder from '../apis/RestaurantFinder';
+import AddReview from '../components/AddReview';
 import Reviews from '../components/Reviews';
 import StarRating from '../components/StarRating';
 import { RestaurantsContext } from '../context/RestaurantsContext';
@@ -13,7 +14,7 @@ const RestaurantDetailPage = () => {
         const fetchData = async () => {
             try {
                 const response = await RestaurantFinder.get(`/${id}`);
-                setSelectedRestaurant(response.data.data.restaurant);
+                setSelectedRestaurant(response.data.data);
             } catch (err) {
                 console.log(err)
             }
@@ -21,13 +22,17 @@ const RestaurantDetailPage = () => {
 
         fetchData();
     }, []);
-    return <div>{selectedRestaurant && (
+    return ( 
+    
+    <div>{selectedRestaurant && (
         <>
+        <h1 className="text-center display-1">{selectedRestaurant.restaurant.name}</h1>
         <div className="mt-3">
-            <Reviews />
+            <Reviews reviews={selectedRestaurant.reviews} />
         </div>
+        <AddReview />
         </>
-    )}</div>;
-};
+    )}</div>
+    )};
 
 export default RestaurantDetailPage;
