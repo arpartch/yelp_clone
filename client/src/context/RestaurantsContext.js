cd4f1cf8ed291b3b0663635b875bb3c2073f604c
Athena <athena.partch@gmail.com>
2021-03-27 08:37:52 -0700

    detail page render

--- client/src/context/RestaurantsContext.js
+++ client/src/context/RestaurantsContext.js
@@ -3,14 +3,15 @@ import React, {useState, createContext} from "react";
 export const RestaurantsContext = createContext();
 
 export const RestaurantsContextProvider = props => {
-        const [restaurants, setRestaurants] = useState([])
+        const [restaurants, setRestaurants] = useState([]);
+        const [selectedRestaurant, setSelectedRestaurant] = useState(null);
 
 
         const addRestaurants = (restaurant) => {
             setRestaurants([...restaurants, restaurant]);
         }
     return(
-        <RestaurantsContext.Provider value={{restaurants, setRestaurants, addRestaurants}}>
+        <RestaurantsContext.Provider value={{restaurants, setRestaurants, addRestaurants, selectedRestaurant, setSelectedRestaurant }}>
             {props.children}
         </RestaurantsContext.Provider>
     )
--- client/src/routes/restaurantDetailPage.jsx
+++ client/src/routes/restaurantDetailPage.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import React, { useContext, useEffect } from 'react';
+import { useParams } from 'react-router';
+import RestaurantFinder from '../apis/RestaurantFinder';
+import { RestaurantsContext } from '../context/RestaurantsContext';
 
 const RestaurantDetailPage = () => {
-    return (
-        <div>
-            Detail Page
-        </div>
-    )
-}
+    const {id} = useParams()
+    const {selectedRestaurant, setSelectedRestaurant} = useContext(RestaurantsContext)
 
-export default RestaurantDetailPage
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await RestaurantFinder.get(`/${id}`);
+                setSelectedRestaurant(response.data.data.restaurant);
+            } catch (err) {
+                console.log(err)
+            }
+        };
+
+        fetchData();
+    }, []);
+    return <div>{selectedRestaurant && selectedRestaurant.name}</div>;
+};
+
+export default RestaurantDetailPage;
