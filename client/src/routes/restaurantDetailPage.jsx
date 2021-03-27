e294109a97f08af20ddf92435816fa8498f0f954
Athena <athena.partch@gmail.com>
2021-03-27 06:11:21 -0700

    add star rating

--- client/src/routes/restaurantDetailPage.jsx
+++ client/src/routes/restaurantDetailPage.jsx
@@ -1,11 +1,31 @@
-import React from 'react'
+import React, { useContext, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { RestaurantsContext } from "../context/RestaurantsContext";
+import RestaurantFinder from "../apis/RestaurantFinder";
+import StarRating from "../components/StarRating";
 
-const restaurantDetailPage = () => {
-    return (
-        <div>
-            Detail Page
-        </div>
-    )
-}
+const RestaurantDetailPage = () => {
+  const { id } = useParams();
+  const { selectedRestaurant, setSelectedRestaurant } = useContext(
+    RestaurantsContext
+  );
 
-export default restaurantDetailPage
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await RestaurantFinder.get(`/${id}`);
+        console.log(response);
+
+        setSelectedRestaurant(response.data.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    fetchData();
+  }, []);
+  return <div>{selectedRestaurant && <StarRating rating={1.2}/>}</div>
+  
+};
+
+export default RestaurantDetailPage;
